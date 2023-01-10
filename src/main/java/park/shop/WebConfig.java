7e3097d0b54d272080040aaff4d5f5d053e3f4e7
park kang lim <gr1993@naver.com>
2023-01-10 17:25:47 +0900

    오류 시 오류 페이지로 가도록 추가

--- src/main/java/park/shop/WebConfig.java
+++ src/main/java/park/shop/WebConfig.java
@@ -2,8 +2,10 @@ package park.shop;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import park.shop.web.argumentresolver.LoginMemberArgumentResolver;
+import park.shop.web.exceptionresolver.MyHandlerExceptionResolver;
 
 import java.util.List;
 
@@ -13,5 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
     }
 }
---
+++ src/main/java/park/shop/web/exceptionresolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,21 @@
+package park.shop.web.exceptionresolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        log.info("500에러 처리");
+
+        request.setAttribute("message", ex.getMessage());
+
+        return new ModelAndView("error/500");
+    }
+}
